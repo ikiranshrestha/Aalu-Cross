ba9cbff88bab01e88f3b32a510895739462414dd
Kiran Shrestha <kiranshrestha903@gmail.com>
2020-08-30 16:41:29 +0545

    First Push

---
+++ app/src/main/java/np/com/shresthakiran/aalucross/MainActivity.java
@@ -0,0 +1,92 @@
+package np.com.shresthakiran.aalucross;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    boolean gameActive = true;
+    //    Player Representation
+//    0 - X
+//    1 - Aalu
+    int activePlayer = 0;
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+// State   Player
+//    0 == X
+//    1 == aalu
+//    2 = NULL
+
+    //     Win Conditions
+    int[][] winPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+            {0, 4, 8}, {2, 4, 6}};
+
+    public void checkboxTapped(View view) {
+        ImageView img = (ImageView) view;
+        int tappedImage = Integer.parseInt(img.getTag().toString());
+
+        if (gameState[tappedImage] == 2) {
+            gameState[tappedImage] = activePlayer;
+            img.setTranslationY(-1000f);
+            if (activePlayer == 0) {
+                img.setImageResource(R.drawable.x);
+                activePlayer = 1;
+                TextView status = findViewById(R.id.tvStatusBar);
+                status.setText("Aalu's Turn - Tap to play");
+            } else {
+                img.setImageResource(R.drawable.aalu);
+                activePlayer = 0;
+                TextView tvstatusBar = findViewById(R.id.tvStatusBar);
+                tvstatusBar.setText("X's Turn - Tap to play");
+            }
+            img.animate().translationYBy(1000f).setDuration(300);
+        }
+        for(int[] winPosition: winPositions){
+            if(gameState[winPosition[0]] == gameState[winPosition[1]]
+                    && gameState[winPosition[1]] == gameState[winPosition[2]]
+                    && gameState[winPosition[0]] != 2){
+                String winnerStr;
+                if(gameState[winPosition[0]] == 0){
+                    winnerStr = "X has won";
+                }
+                else{
+                    winnerStr = "Aalu has won";
+                }
+                TextView tvstatusBar = findViewById(R.id.tvStatusBar);
+                tvstatusBar.setText(winnerStr);
+            }
+        }
+    }
+
+    public void gameReset(View view) {
+        gameActive = true;
+        activePlayer = 0;
+        for (int i = 0; i < gameState.length; i++) {
+            gameState[i] = 2;
+        }
+        ((ImageView) findViewById(R.id.imageView0)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView1)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView2)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView3)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView4)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView5)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView6)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView7)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView8)).setImageResource(0);
+
+        TextView status = findViewById(R.id.tvStatusBar);
+        status.setText("X's Turn - Tap to play");
+
+    }
+
+    // Win check
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
